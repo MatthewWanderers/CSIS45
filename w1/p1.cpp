112551fa659cd3ddad67b17a08d90dffa96bc1cc
Matthew <matthewmichaelmartinez@gmail.com>
2018-08-28 21:34:48 -0700

    make p1

---
+++ w1/p1.cpp
@@ -0,0 +1,28 @@
+// Matthew Martinez
+// Extra Credit 1:Blown to Bits.doc
+// 08/28/2018 - 08/28/2018
+// https://www.youtube.com/watch?v=ntY01qoIdus
+// Type up the sample C++ program below including the comments.
+// Once the program is working, append your program test run at the end of your
+// code as a multiline comment, as shown in the sample program!
+// Save and submit it as p1.cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello";
+  cout << "\n";
+  cout << "Human" << "\n";
+
+  string myName = "Matthew";
+  cout << "Your name is: " << myName << "\n";
+
+  int wholeNum = 5.0;
+  cout << "The number stored in wholeNum is: " << wholeNum << "\n";
+
+  float myFloat = 5.0;
+  cout << "The number stored in myFloat is: " << myFloat << "\n";
+
+  return 0;
+}
